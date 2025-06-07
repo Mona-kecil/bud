d423c311ce17cbee7184da90c9600255f67b2357
Mona-kecil <flowyourminds@gmail.com>
2025-06-08 01:07:00 +0700

    fix statsig wrong workspace error

--- src/components/statsig-client-provider.tsx
+++ src/components/statsig-client-provider.tsx
@@ -3,18 +3,17 @@
 "use client";
 
 import React from "react";
-import { StatsigProvider, useClientAsyncInit } from "@statsig/react-bindings";
-import { StatsigAutoCapturePlugin } from "@statsig/web-analytics";
-import { StatsigSessionReplayPlugin } from "@statsig/session-replay";
+import { StatsigProvider, useClientAsyncInit } from '@statsig/react-bindings';
+import { StatsigAutoCapturePlugin } from '@statsig/web-analytics';
+import { StatsigSessionReplayPlugin } from '@statsig/session-replay';
 
-export default function StatsigClientProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+
+export default function MyStatsig({ children }: { children: React.ReactNode }) {
   const { client } = useClientAsyncInit(
-    "client-OCNEiCn3GD8kvNm7UzKWpF7QcPca41ao61HhenGs7xE",
-    { userID: "a-user" },
+    "client-m4KLqCdQLLSp7SkjoI4Pt03z53glCowHUezyCUBIfVh",
+    {
+      userID: "a-user",
+    },
     {
       plugins: [
         new StatsigAutoCapturePlugin(),
