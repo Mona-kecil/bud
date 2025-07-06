4738572e993f99a46fd8ddb482635c9f554dc2a2
Mona-kecil <flowyourminds@gmail.com>
2025-07-06 14:28:44 +0700

    add profile button

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -4,6 +4,7 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "~/components/ui/sidebar";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Sidebar({ children }: { children: React.ReactNode }) {
   return (
@@ -11,9 +12,9 @@ export default function Sidebar({ children }: { children: React.ReactNode }) {
       <AppSidebar />
       <SidebarInset>
         <header className="bg-accent sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
+          <div className="flex w-full items-center justify-between gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
-            <h1 className="text-2xl font-bold">Bud</h1>
+            <UserButton />
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
