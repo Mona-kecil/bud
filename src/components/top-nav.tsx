eb8090d62e0fd63bbe0b9ae8572ba3e9bc0e905c
Mona-kecil <flowyourminds@gmail.com>
2025-08-16 17:02:28 +0800

    add top-nav component

---
+++ src/components/top-nav.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+import { Settings } from "lucide-react";
+import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
+import { toast } from "sonner";
+
+export default function TopNav() {
+  const pathname = usePathname();
+  const currentPageTitle = pathname.slice(1, 2).toLocaleUpperCase() + pathname.slice(2);
+
+  const onSettingButtonClicked = () => {
+    toast.message("We don't have this feature yet 😭")
+  }
+
+  return (
+    <div className="w-full flex justify-between items-center">
+      {/* settings button */}
+      <Button variant="ghost" size={"icon"} onClick={onSettingButtonClicked}>
+        <Settings />
+      </Button>
+      {/* current page title*/}
+      <h1 className="font-bold text-xl">{currentPageTitle}</h1>
+      {/* user button */}
+      <UserButton />
+    </div>
+  )
+}
